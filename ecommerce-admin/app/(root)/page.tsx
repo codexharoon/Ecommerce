c160b71f6d8a2b334598f437122873a837f91360
Muhammad Haroon <theharoonsocial@gmail.com>
2024-09-21 21:54:05 +0500

    Nextjs + Shadcn ui

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex items-center gap-3 p-4">
+      <h1>Admin Dashboard</h1>
+      <Button>Click Me!</Button>
+    </div>
+  );
+}
