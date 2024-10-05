1b02590f6c5c36307ccedd8856fe5cc6a4e3dc3a
Muhammad Haroon <theharoonsocial@gmail.com>
2024-10-05 17:08:37 +0500

    Change shadcn-theme

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -23,7 +23,7 @@ export function MainNav({ className }: { className?: string }) {
           key={route.href}
           href={route.href}
           className={cn(
-            "text-sm font-medium transition-colors hover:text-primary-foreground",
+            "text-sm font-medium transition-colors hover:text-primary",
             route.active
               ? "text-black dark:text-white"
               : "text-muted-foreground"
