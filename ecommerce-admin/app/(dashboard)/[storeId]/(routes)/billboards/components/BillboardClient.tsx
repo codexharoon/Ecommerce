0770dbb75614f3e1e4e25d758cf227d088babf9b
Muhammad Haroon <theharoonsocial@gmail.com>
2024-10-09 12:30:54 +0500

    data table for billboards

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/BillboardClient.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/BillboardClient.tsx
@@ -3,12 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/Heading";
 import { Separator } from "@/components/ui/separator";
-import { Billboard } from "@prisma/client";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
 interface BillboardClientProps {
-  billboards: Billboard[];
+  billboards: BillboardColumn[];
 }
 
 const BillboardClient = ({ billboards }: BillboardClientProps) => {
@@ -31,6 +32,8 @@ const BillboardClient = ({ billboards }: BillboardClientProps) => {
       </div>
 
       <Separator />
+
+      <DataTable columns={columns} data={billboards} />
     </>
   );
 };
--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 import BillboardClient from "./components/BillboardClient";
+import { BillboardColumn } from "./components/columns";
+import { format } from "date-fns";
 
 const page = async ({ params }: { params: { storeId: string } }) => {
   const billboards = await prisma.billboard.findMany({
@@ -11,10 +13,18 @@ const page = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (billboard) => ({
+      id: billboard.id,
+      label: billboard.label,
+      createdAt: format(billboard.createdAt, "MMMM do,yyyy"),
+    })
+  );
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient billboards={billboards} />
+        <BillboardClient billboards={formattedBillboards} />
       </div>
     </div>
   );
