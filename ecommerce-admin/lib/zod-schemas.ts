03da6ab24e28f0a58833cbdd72d5f7a2442fcc64
Muhammad Haroon <theharoonsocial@gmail.com>
2024-10-16 18:15:32 +0500

    readable messages in zod schemas

--- ecommerce-admin/lib/zod-schemas.ts
+++ ecommerce-admin/lib/zod-schemas.ts
@@ -1,26 +1,26 @@
 import * as z from "zod";
 
 export const storeModelSchema = z.object({
-  name: z.string().min(2).max(50),
+  name: z.string().min(2).max(50, { message: "Name is required" }),
 });
 
 export const billboardModelSchema = z.object({
-  label: z.string().min(2).max(50),
-  imageUrl: z.string().min(1),
+  label: z.string().min(2).max(50, { message: "Label is required" }),
+  imageUrl: z.string().min(1, { message: "Image is required" }),
 });
 
 export const CategoriesModelSchema = z.object({
-  name: z.string().min(2).max(50),
-  billboardId: z.string().min(1),
+  name: z.string().min(2).max(50, { message: "Name is required" }),
+  billboardId: z.string().min(1, { message: "Billboard is required" }),
 });
 
 export const SizeModelSchema = z.object({
-  name: z.string().min(2).max(20),
-  value: z.string().min(1).max(10),
+  name: z.string().min(2).max(20, { message: "Name is required" }),
+  value: z.string().min(1).max(10, { message: "Value is required" }),
 });
 
 export const ColorModelSchema = z.object({
-  name: z.string().min(2).max(20),
+  name: z.string().min(2).max(20, { message: "Name is required" }),
   value: z
     .string()
     .min(3)
@@ -31,16 +31,17 @@ export const ColorModelSchema = z.object({
 });
 
 export const ProductModelSchema = z.object({
-  name: z.string().min(2).max(50),
-  price: z.coerce.number().min(1),
+  name: z.string().min(2).max(50, { message: "Name is required" }),
+  price: z.coerce.number().min(1, { message: "Price is required" }),
   isFeatured: z.boolean().default(false).optional(),
   isArchived: z.boolean().default(false).optional(),
-  categoryId: z.string().min(1),
-  sizeId: z.string().min(1),
-  colorId: z.string().min(1),
+  categoryId: z.string().min(1, { message: "Category is required" }),
+  sizeId: z.string().min(1, { message: "Size is required" }),
+  colorId: z.string().min(1, { message: "Color is required" }),
   images: z
     .object({
       url: z.string(),
     })
-    .array(),
+    .array()
+    .min(1, { message: "At least one image is required" }),
 });
