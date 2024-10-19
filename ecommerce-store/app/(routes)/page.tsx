aa41a7d556ff05a8302a1c2df109596552f22386
Muhammad Haroon <theharoonsocial@gmail.com>
2024-10-19 17:19:41 +0500

    billboard section in homepage

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const page = () => {
-  return <div>home page</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("cm2g4cyn80001b7mbbgjlackm");
+  return (
+    <div>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
-export default page;
+export default HomePage;
